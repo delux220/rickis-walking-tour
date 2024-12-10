1d78cd2c60d8dcef6fd1c6cf628a9664c0bd3139
delux220 <dave.luke@gmail.com>
2024-12-10 16:57:12 -0500

    changes

--- src/pages/index.he.js
+++ src/pages/index.he.js
@@ -189,7 +189,7 @@ const IndexPage = ({data}) => {
 		<section className="bg-white">
 			<div className="container mx-auto   sm:px-6">
 				<div className=" px-4 sm:px-8 py-4 sm:pt-8">
-					<h2 className="w-full block bg-light text-black md:bg-transparent md:border-0 px-4 py-4 text-center font-epicursive md:text-gold text-4xl md:text-6xl md:mb-8 mx-auto on-scroll" >ברוכים הבאים<span className="block">לניו-יורק שלא</span>הכרתם</h2>
+					<h1 className="w-full block bg-light text-black md:bg-transparent md:border-0 px-4 py-4 text-center font-epicursive md:text-gold text-4xl md:text-6xl md:mb-8 mx-auto on-scroll" >Welcome to New York</h1>
 					<h3 className="text-center hidden text-gold font-goodlife font-bold text-3xl mb-4 uppercase font-bold ">המדריכה שלכם לניו יורק סיטי</h3>
 					<h3 className="text-center hidden text-black  text-lg font-sans mb-8 block xl:max-w-lg">הצטרפו אלי לסימטאות מלאות ההיסטוריה של ניו יורק, לטעום מהמאכלים הטובים ביותר וליהנות מהעיר ללא הפסקה.</h3>
 					
